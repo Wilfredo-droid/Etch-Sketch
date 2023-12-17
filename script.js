1e745def3fec7221c340c4f5791977f25e568f49
Wilfredo-droid <wilfredoandres10@gmail.com>
2023-12-17 17:36:27 -0400

    Make paint grid to be a function to fix a bug related to changing grid
    Painting the grid only worked once, make it into a function to reuse it
    when changing the grid

--- script.js
+++ script.js
@@ -22,47 +22,53 @@ let makeGrid = (rows = 16, columns = 16) => {
 
 }
 
+let fillGrid = () => {
+
 //A lot of thanks to zhirzh on a stackoverflow article for his code snipped which helped me in the following code, Marcus, you wont be forgotten
 
 /*https://stackoverflow.com/questions/48593312/javascript-event-when-mouseover-and-mousedown*/
 
-
 /*The shared state variable managing to keep the three event listeners together, without it, the three event listeners would work independently and not work */
-let isHolding = false;
+    let isHolding = false;
 
-//The mouse down has to be on the grid instead of the columns
-grid.addEventListener("mousedown", () => {
-    isHolding = true;
-});
+    //The mouse down has to be on the grid instead of the columns
+    grid.addEventListener("mousedown", () => {
+        isHolding = true;
+    });
 
-//Thanks to Borislav Hadzhiev for the following code on their blogpost https://bobbyhadz.com/blog/disable-drag-and-drop-in-javascript
-grid.addEventListener("dragstart", (e) => {
-    e.preventDefault();
-})
+    let columns = document.querySelectorAll(".column");
 
-grid.addEventListener("drop", (e) => {
-    e.preventDefault();
-})
-
-let columns = document.querySelectorAll(".column");
-
-columns.forEach(column => {
+    columns.forEach(column => {
 
 
-    column.addEventListener("mouseenter", () => {
-        
-        if(isHolding === true){
-            column.style.backgroundColor = "black";
-        }
+        column.addEventListener("mouseenter", () => {
+            
+            if(isHolding === true){
+                column.style.backgroundColor = "black";
+            }
 
+        })
+
+        column.addEventListener("mouseup", () => {
+            isHolding = false;
+        })
+
+
+    });
+
+//Code to prevent drag
+    
+    //Thanks to Borislav Hadzhiev for the following code on their blogpost https://bobbyhadz.com/blog/disable-drag-and-drop-in-javascript
+    grid.addEventListener("dragstart", (e) => {
+        e.preventDefault();
     })
 
-    column.addEventListener("mouseup", () => {
-        isHolding = false;
+    grid.addEventListener("drop", (e) => {
+        e.preventDefault();
     })
 
 
-});
+}
 
 let changeGrid = document.querySelector('.changeGrid');
 
@@ -80,6 +86,8 @@ changeGrid.addEventListener("click", () => {
     else{
         makeGrid(numberRows,numberColumns);
         changeGrid.innerText += ` ${numberRows} x ${numberColumns}`
+
+        fillGrid();
     }
 
 });
