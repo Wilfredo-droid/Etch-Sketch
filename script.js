b1542364d16bf13bffb5053e3c2a580ff80f0046
Wilfredo-droid <wilfredoandres10@gmail.com>
2023-12-17 12:52:45 -0400

    Create rows with js inside loop

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let grid = document.querySelector("#grid");
 
 for(let i = 0; i < 16; i++){
 
-   
+    let row = document.createElement('div');
+    row.classList.add("row");
 
     for(let j = 0; j < 16; j++){
     
     }
 
+    grid.appendChild(row);
 
 }
